3d6255e619b0463aeb3f0e84672eeb23e6181469
RobbieGM <rgm020103@gmail.com>
2019-12-02 17:21:47 -0500

    Temporarily giving up on server testing

--- src/backend/testing/setup-test-api-server.ts
+++ src/backend/testing/setup-test-api-server.ts
@@ -6,36 +6,14 @@ import { applyApiServerMiddleware } from '../api-middleware';
 const app = express();
 
 export default async function startApiTestServer() {
-  try {
-    await applyApiServerMiddleware(app);
-    global.TEST_SERVER = app.listen({ port: 5001 });
-  } catch (e) {
-    if (e instanceof GeneratingSchemaError) {
-      console.error(e.details[0]);
-    } else {
-      console.error(e);
-    }
-  }
+  // try {
+  //   await applyApiServerMiddleware(app);
+  //   global.TEST_SERVER = app.listen({ port: 5001 });
+  // } catch (e) {
+  //   if (e instanceof GeneratingSchemaError) {
+  //     console.error(e.details[0]);
+  //   } else {
+  //     console.error(e);
+  //   }
+  // }
 }
-
-// import { spawn } from 'child_process';
-// import express from 'express';
-
-// export default function startApiTestServer() {
-//   return new Promise<void>((res, rej) => {
-//     const npm = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
-//     global.TEST_SERVER = spawn(npm, ['run', 'backend-dev', '-p', '5001'], {
-//       stdio: 'inherit',
-//       detached: true,
-//       env: {
-//         ...process.env,
-//         PORT: '5001'
-//       }
-//     });
-//     global.TEST_SERVER.stdout?.on('data', data => {
-//       if (data.toString().includes('Ready')) {
-//         res();
-//       }
-//     });
-//   });
-// }
